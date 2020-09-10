98ddd405f127b976519f19f6152f55bca61f336f
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-09-10 11:51:28 +0200

    GamesList constructor added

--- src/GamesList.js
+++ src/GamesList.js
@@ -1,17 +1,20 @@
 import React from "react";
-import gamesListSource from "./games-list.json";
+import gamesListData from "./games-list.json";
 
 class GamesList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gamesList: gamesListData.slice(),
+    };
+  }
+
   render() {
     return (
       <div className="gameslist">
         <h1>Games</h1>
-        <div className="buttons">
-          {/* <Buttons /> */}
-        </div>
-        <div className="games">
-          {/* <Games /> */}
-        </div>
+        <div className="buttons">{/* <Buttons /> */}</div>
+        <div className="games">{/* <Games /> */}</div>
       </div>
     );
   }
