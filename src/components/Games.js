a8581a3b39c6973676b946a3dc8227e53a60a481
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-09-17 11:26:15 +0200

    Games list displayed

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React from "react";
 
 class Games extends React.Component {
   render() {
-    const gamesListSize = this.props.gamesList.length;
+    // const gamesListSize = this.props.gamesList.length;
+    const gamesList = this.props.gamesList;
+    const gamesCards = gamesList.map((game, index) => {
+      return (
+        <div key={index} className="game">
+          <a href={`/game/${game.ID}`}>
+            <img src="https://picsum.photos/200/300" alt="" />
+          </a>
+          <h2>{game.Name}</h2>
+          <p>
+            Platform : {game.Platform}
+            <br />
+            Year : {game.Year}
+            <br />
+            Genre : {game.Genre}
+            <br />
+            Publisher : {game.Publisher}
+          </p>
+        </div>
+      );
+    });
 
-  return (
-    <div>
-      Game: {gamesListSize}
-    </div>
-    );
+    return <div>{gamesCards}</div>;
   }
 }
 
