a13153f51452594fa3809cdcd4a4c3c926327b2c
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-09-18 13:59:29 +0200

    Axios installed and GamesList updated to use it

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,13 +1,23 @@
 import React from "react";
-import gamesListData from "../data/games-list.json";
+// import gamesListData from "../data/games-list.json";
 import Games from "./Games";
+import axios from "axios";
 
 class GamesList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      gamesList: gamesListData,
+      // gamesList: gamesListData,
+      gamesList: [],
     };
+  }
+
+  componentDidMount() {
+    axios.get(`https://localhost:8000/games`).then((res) => {
+      console.log("res", res);
+      const gamesList = res.data;
+      this.setState({ gamesList });
+    });
   }
 
   render() {
@@ -15,7 +25,9 @@ class GamesList extends React.Component {
       <div className="gameslist">
         <h1>Games</h1>
         <div className="buttons">{/* <Buttons /> */}</div>
-        <div className="games"><Games gamesList={this.state.gamesList}/></div>
+        <div className="games">
+          <Games gamesList={this.state.gamesList} />
+        </div>
       </div>
     );
   }
