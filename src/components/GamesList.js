ef8a7c6615cb2e7504c42d9da9fb817bc44d463e
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-09-21 14:18:55 +0200

    catch typo fixed

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -13,26 +13,27 @@ class GamesList extends React.Component {
   }
 
   componentDidMount() {
-    axios.get("https://localhost:8000/games").then((res) => {
-      console.clear();
-      console.log("Res", res);
-      const gamesList = res.data;
-      this.setState({ gamesList }).catch((res) => {
+    axios
+      .get("https://localhost:8000/games")
+      .then((res) => {
+        console.clear();
+        console.log("Res", res);
+        const gamesList = res.data;
+        this.setState({ gamesList });
+      })
+      .catch((res) => {
         console.clear();
         console.log(res);
       });
-    });
   }
 
   render() {
     return (
-      <div className="gameslist">
+      <>
         <h1>Games</h1>
-        <div className="buttons">{/* <Buttons /> */}</div>
-        <div className="games">
-          <Games gamesList={this.state.gamesList} />
-        </div>
-      </div>
+        {/* <Buttons /> */}
+        <Games gamesList={this.state.gamesList} />
+      </>
     );
   }
 }
