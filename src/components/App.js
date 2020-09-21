dee09ef84ce9623352dbf0607bc0139e57c8a545
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-09-21 13:54:37 +0200

    App tranformed to an functional component

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,17 @@ import Game from "./Game";
 import { Switch, Route } from "react-router-dom";
 import GamesList from "./GamesList";
 
-export default function App() {
-  return (
-    <div>
-      <Switch>
-        <Route path="/game/:id">
-          <Game />
-        </Route>
-        <Route path="/">
-          <GamesList />
-        </Route>
-      </Switch>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Switch>
+      <Route path="/game/:id">
+        <Game />
+      </Route>
+      <Route path="/">
+        <GamesList />
+      </Route>
+    </Switch>
+  </div>
+);
+
+export default App;
